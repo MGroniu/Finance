7ef2f860330c1a3d28246c20fe95de3774c5bac2
Mikołaj <mikolaj.gronek@gmail.com>
2019-01-27 23:19:30 +0100

    push

---
+++ src/EuropeOption.cpp
@@ -0,0 +1,53 @@
+#include<cmath>
+#include<iostream>
+#include "EuropeOption.h"
+#include"Random1.h"
+
+
+//definition of constructor
+EuropeOption::EuropeOption(
+    double Barrier,
+	int nInt_,
+	double strike_,
+	double spot_,
+	double vol_,
+	double r_,
+	double expiry_){
+	  Barier = Barier_;
+		nInt = nInt_;
+		strike = strike_;
+		spot = spot_;
+		vol = vol_;
+		r = r_;
+		expiry = expiry_;
+		generatePath();
+}
+
+//method definition
+void EuropeOption::generatePath(){
+	double thisDrift = (r * expiry - 0.5 * vol * vol * expiry) / double(nInt);
+	double cumShocks = 0;
+	thisPath.clear();
+
+	for(int i = 0; i < nInt; i++){
+		cumShocks += (thisDrift + vol * sqrt(expiry / double(nInt)) * GetOneGaussianByBoxMuller());
+		thisPath.push_back(spot * exp(cumShocks));
+	}
+}
+
+double EuropeOption::getdownandoutput(int nReps){
+
+	double rollingSum = 0.0;
+	double thisMin = 0.0;
+
+	for(int i = 0; i < nReps; i++){f
+		generatePath();
+		thisMin = std::min_element(thisPath.begin(), thisPath.end());
+		if(thisMin>Barrier){
+            rollingSum += (thisPath.back() < strike) ? (strike - thisPath.back()) : 0;
+		}
+	}
+
+	return exp(-r * expiry) * rollingSum / double(nReps);
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include<Rcpp.h>
+#include"EuropeOption.h"
+#include<cmath>
+#include<iostream>
+#include"Random1.h"
+
+using std::vector;
+
+using namespace Rcpp;
+
+
+
+// [[Rcpp::export]]
+
+double getdownandoutput(int nInt = 0,
+
+							  double strike = 100,
+
+                              double spot = 95,
+
+                              double vol = 0.2,
+
+                              double r = 0.06,
+
+                              double expiry = 0.5,
+
+							  double Barrier = 93,
+
+                              int nReps = 800){
+
+
+
+
+	EuropeOption OptionOne(nInt, strike, spot, vol, r, expiry, Barrier);
+
+
+	double price =	OptionOne.getdownandoutput(nReps);
+
+
+	return price;
+
+}
